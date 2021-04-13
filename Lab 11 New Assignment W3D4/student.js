83cd8697334b1eb5949187a45363ef2315a5b286
michaelgalazar <79475036+michaelgalazar@users.noreply.github.com>
2021-04-12 21:43:14 -0700

    Assignment

--- Lab 11 New Assignment W3D4/student.js
+++ Lab 11 New Assignment W3D4/student.js
@@ -82,4 +82,4 @@ for ( let eachStudent of newArrayOfStudents ) {
 newArrayOfStudents.sort = Array.prototype.sort;
 
 newArrayOfStudents.sort();
-console.log(newArrayOfStudents.sort);
+console.log(newArrayOfStudents.sort());
